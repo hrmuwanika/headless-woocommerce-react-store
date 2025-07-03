d89cceab32285d11118b3a8df4cb7f484507ce8f
Henry Robert Muwanika <hrmuwanika@gmail.com>
2025-07-03 12:16:46 +0200

    Update NavBar.jsx

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -9,6 +9,7 @@ const NavBar = ({ cartItem, isAuthenticated, setUserLogout }) => {
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
+        </div>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
@@ -41,7 +42,6 @@ const NavBar = ({ cartItem, isAuthenticated, setUserLogout }) => {
               ) 
             }
           </ul>
-        </div>
         </div>
       </nav>
     </>
